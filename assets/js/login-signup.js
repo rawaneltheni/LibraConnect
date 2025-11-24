e9b8151daac9eb392e57f6e5f165470b12f9ce8d
rawaneltheni <rawaneltheni@gmail.com>
2025-11-24 18:46:02 +0200

    Check limu domain

--- assets/js/login-signup.js
+++ assets/js/login-signup.js
@@ -1,3 +1,5 @@
+// ==================== SignUp / SignIn slider ====================
+
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
@@ -8,4 +10,25 @@ signUpButton.addEventListener('click', () => {
 
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
+});
+
+
+// ==================== Check @limu.edu.ly ====================
+
+const forms = document.querySelectorAll("form");
+
+forms.forEach(form => {
+    form.addEventListener("submit", function(event) {
+      	event.preventDefault(); // stop the default redirect
+
+		const emailInput = form.querySelector('input[type="email"]');
+		const email = emailInput.value.trim();
+
+		// Check if email ends with @limu.edu.ly
+		if (/@limu\.edu\.ly$/i.test(email)) {
+			// Email is valid, redirect to index.html
+			window.location.href = "index.html";
+		} else alert("Please use a valid @limu.edu.ly email.");
+		
+    });
 });
